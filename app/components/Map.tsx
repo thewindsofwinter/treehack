d401f86d22a10a1d6477de0a1fe011528fc71369
Jenny Wei <jenwwwww@gmail.com>
2023-09-16 22:37:05 -0400

    update map to use google maps api

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,26 +1,61 @@
 "use client"
-import { MapContainer, TileLayer } from 'react-leaflet'
-import Label from './../components/Label'
+// import { MapContainer, TileLayer } from 'react-leaflet'
+// import Label from './../components/Label'
+// import React from 'react'
+import { GoogleMap, LoadScript, OverlayView } from '@react-google-maps/api';
 
 // const fetch = require('node-fetch'); // For Node.js environment
 const apiKey = "AIzaSyCwEgxhHsfCIZz9rRDOHvwpHQmTnhv8osk" // move this later
 
-const Map = () =>{
-    return (
-      <MapContainer id="Start" center={[34.1083,-117.2898]} zoom={5}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <TileLayer
-                url={`https://airquality.googleapis.com/v1/mapTypes/US_AQI/heatmapTiles/{z}/{x}/{y}?key=${apiKey}`}
-                opacity = {0.5}
-        />
-<Label text={"Hello"} latlng={[34.1083,-117.2898]}/>
+// const Map = () =>{
+//     return (
 
-      </MapContainer>
-    )
+//       <MapContainer id="Start" center={[34.1083,-117.2898]} zoom={5}>
+//         <TileLayer
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         />
+//         <TileLayer
+//                 url={`https://airquality.googleapis.com/v1/mapTypes/US_AQI/heatmapTiles/{z}/{x}/{y}?key=${apiKey}`}
+//                 opacity = {0.5}
+//         />
+// <Label text={"Hello"} latlng={[34.1083,-117.2898]}/>
+
+//       </MapContainer>
+//     )
+// }
+
+const center = {
+  lat: 34.1083,
+  lng: -117.2898
+};
+
+const Map = () => {
+  return (
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        id="map"
+        center={center}
+        zoom={5}
+        options={{
+          // mapTypeId: 'hybrid',
+          streetViewControl: false,
+          // mapTypeControl: false
+        }}
+      >
+      {/* <OverlayView
+          position={center}
+          mapPaneName={OverlayView.OVERLAY_LAYER}
+      >
+          <img 
+            src={`https://airquality.googleapis.com/v1/mapTypes/US_AQI/heatmapTiles/5/5/5?key=${apiKey}`} 
+            alt="Air Quality" 
+            style={{ opacity: 0.5 }} 
+          />
+        </OverlayView> */}
+      </GoogleMap>
+    </LoadScript>
+  );
 }
-
 
 export default Map
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Splash from './components/Splash'
+import Map from "./components/Map"
 import dynamic from 'next/dynamic'
 
 export default function Home() {
 
-  const Map = dynamic(() => import("./components/Map"), { ssr: false });
+  //const Map = dynamic(() => import("./components/Map"), { ssr: false });
 
   return (
     <>
