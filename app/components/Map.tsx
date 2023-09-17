676140fd43a83c07fdc04357e5a3fc1b3a329f15
Jenny Wei <jenwwwww@gmail.com>
2023-09-16 20:28:52 -0400

    add map overlay

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -4,14 +4,16 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 // const fetch = require('node-fetch'); // For Node.js environment
 const apiKey = "AIzaSyCwEgxhHsfCIZz9rRDOHvwpHQmTnhv8osk" // move this later
 
-const endpoint = `https://airquality.googleapis.com/v1/mapTypes/TYPE/heatmapTiles/2/0/1?key=${apiKey}`
-
 const Map = () =>{
     return (
       <MapContainer id="Start" center={[34.1083,-117.2898]} zoom={5}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <TileLayer
+                url={`https://airquality.googleapis.com/v1/mapTypes/US_AQI/heatmapTiles/{z}/{x}/{y}?key=${apiKey}`}
+                opacity = {0.5}
         />
         <Marker position={[34.1083,-117.2898]}>
           <Popup>
--- app/components/Popup.tsx
+++ app/components/Popup.tsx
@@ -1,11 +1,11 @@
 import { Marker, Popup } from 'react-leaflet'
 
-export default () =>{
-    return (
-        <Marker position={[34.1083,-117.2898]}>
-            <Popup>
-                Bad Canopy C
-            </Popup>
-        </Marker>
-    )
-}
+// export default () =>{
+//     return (
+//         <Marker position={[34.1083,-117.2898]}>
+//             <Popup>
+//                 Bad Canopy C
+//             </Popup>
+//         </Marker>
+//     )
+// }
