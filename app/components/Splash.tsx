8f113ed78f603e1d7e59a8891c64742706ed2f04
Jenny Wei <jenwwwww@gmail.com>
2023-09-17 06:01:59 -0400

    update styling

--- app/components/Splash.tsx
+++ app/components/Splash.tsx
@@ -9,7 +9,10 @@ const Splash = () =>{
                 <div className="text">
                     <div className="title">TREEHACK</div>
                     <div className="subtitle">Fight the urban heat island effect by visualizing a greener future</div>
-                    <a href='#Start'><button className="hero-button">Get Started</button></a>
+                    <div className="btn-container">
+                        <a href='#Start'><button className="hero-button">Get Started</button></a>
+                        <a href='#About'><button className="hero-button">Learn More</button></a>
+                    </div>
                 </div>
             </div>
         </>
