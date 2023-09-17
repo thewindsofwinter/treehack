036f8120b4d3e01bbe27893a3548884e2b3dba4b
balaji-balachandran <balajimbalachandran@gmail.com>
2023-09-16 20:00:18 -0400

    Popup Component

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -3,22 +3,16 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Map = () =>{
     return (
-      <MapContainer id="Start" center={[51.505, -0.09]} zoom={13} style={{  height: '80vh',
-                                                                 width: '70%',
-                                                                 margin: '10vh',
-                                                                 left: 'calc(15% - 10vh)',
-                                                                 position: 'relative',
-                                                                 borderRadius: '20px'
-                                                                  }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A sample marker!!!!!
-        </Popup>
-      </Marker>
+      <MapContainer id="Start" center={[34.1083,-117.2898]} zoom={5}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[34.1083,-117.2898]}>
+          <Popup>
+            Bad Canopy C
+          </Popup>
+        </Marker>
     </MapContainer>
     )
 }
